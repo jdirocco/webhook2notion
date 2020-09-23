46e3730a02b4140759bf40b1e15c5b26d9bfe09d
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2020-09-23 10:54:06 +0200

    add entry

--- app.py
+++ app.py
@@ -8,52 +8,67 @@ from flask import request
 app = Flask(__name__)
 
 
-def createNotionTask(token, collectionURL, content):
+def add_entry_into_table(token, collectionURL, content):
     # notion
     client = NotionClient(token)
     cv = client.get_collection_view(collectionURL)
-    #row = cv.collection.add_row()
-    #row.Name = content['name']
-    #row.juri = str(content['age'])
+    
     row = cv.collection.add_row()
 
-    for value in cv.collection.get_schema_properties():
-        if value['name'] in content.keys():
-            print(value['name'])
-            if value['type'] in ["text", "title"]:
-                row.set_property(value['name'], content[value['name']])
+    for property in cv.collection.get_schema_properties():
+        if property['name'] in content.keys():
 
-            if value['type'] == "multi_select":
-                row.set_property(value['name'], [content[value['name']]])
-            # the value has to be a defined option
-            if value['type'] == "select":
+			# text, title
+            if property['type'] in ["text", "title"]:
+                row.set_property(property['name'], content[property['name']])
+
+			# multi_select
+            if property['type'] == "multi_select":
+                row.set_property(property['name'], [content[property['name']]])
+
+            # select - the property option must be already defined (consequently try/except is used - to catch undefined option errors)
+            if property['type'] == "select":
                 try:
-                    row.set_property(value['name'], content[value['name']])
+                    row.set_property(property['name'], content[property['name']])
                 except:
-                    print("{} is not a valid option".format(str(content[value['name']])))
-            if value['type'] == "checkbox":
-                row.set_property(value['name'],
-                                 content[value['name']].lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup',
+					# to be done: log error
+                    print("{} is not a valid option".format(str(content[property['name']])))
+            
+			# checkbox
+			if property['type'] == "checkbox":
+                row.set_property(property['name'],
+                                 content[property['name']].lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup',
                                                                  'certainly', 'uh-huh'])
-            if value['type'] == "file":
+            # file ?
+			if property['type'] == "file":
                 # Change the property
-                row.set_property(value['name'], content[value['name']])
-            if value['type'] == "person":
+                row.set_property(property['name'], content[property['name']])
+            
+			# person ?		
+			if property['type'] == "person":
                 # Change the property
-                row.set_property(value['name'], content[value['name']])
-            if value['type'] == "date":
+                row.set_property(property['name'], content[property['name']])
+
+			# date ?
+            if property['type'] == "date":
                 # Change the property
-                row.set_property(value['name'], content[value['name']])
-            # Others ??
+                row.set_property(property['name'], content[property['name']])
+
+			# number
+			if property['type'] == "number":
+				# Change the property
+				row.set_property(property['name'], content[property['name']])
+            
+			# missing types: url, email, phone
 
 
 
-@app.route('/create_todo', methods=['POST'])
-def create_todo():
+@app.route('/add_entry', methods=['POST'])
+def add_entry():
     json_data = request.get_json()
     token_v2 = os.environ.get("TOKEN")
     url = os.environ.get("URL")
-    createNotionTask(token_v2, url, json_data)
+    add_entry_into_table(token_v2, url, json_data)
     return f'OK'
 
 
