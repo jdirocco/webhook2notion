19b48708c1ac0e9dec76de7535f844328e39b41c
md2manoppello <juri.di.rocco@gmail.com>
2020-09-23 09:09:11 +0200

    Automated mappings

--- app.py
+++ app.py
@@ -12,23 +12,45 @@ def createNotionTask(token, collectionURL, content):
     # notion
     client = NotionClient(token)
     cv = client.get_collection_view(collectionURL)
+    #row = cv.collection.add_row()
+    #row.Name = content['name']
+    #row.juri = str(content['age'])
     row = cv.collection.add_row()
-    row.Name = content['name']
-    row.juri = str(content['age'])
+    for value in cv.collection.get_schema_properties():
+        if value['name'] in content.keys():
+            print(value['name'])
+            if value['type'] in ["text", "title"]:
+                row.set_property(value['name'], content[value['name']])
+
+            if value['type'] == "multi_select":
+                row.set_property(value['name'], [content[value['name']]])
+            # the value has to be a defined option
+            if value['type'] == "select":
+                try:
+                    row.set_property(value['name'], content[value['name']])
+                except:
+                    print("{} is not a valid option".format(str(content[value['name']])))
+            if value['type'] == "checkbox":
+                row.set_property(value['name'],
+                                 content[value['name']].lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup',
+                                                                 'certainly', 'uh-huh'])
+            if value['type'] == "file":
+                # Change the property
+                row.set_property(value['name'], content[value['name']])
+            if value['type'] == "person":
+                # Change the property
+                row.set_property(value['name'], content[value['name']])
+            if value['type'] == "date":
+                # Change the property
+                row.set_property(value['name'], content[value['name']])
+            # Others ??
 
 
 @app.route('/create_todo', methods=['POST'])
 def create_todo():
     json_data = request.get_json()
-    print(json_data)
-    print(json_data['name'])
-    print(json_data['age'])
-    print(json_data['pippo'])
-    #todo = request.args.get('todo')
     token_v2 = os.environ.get("TOKEN")
     url = os.environ.get("URL")
-    print(token_v2)
-    print(url)
     createNotionTask(token_v2, url, json_data)
     return f'Juri to PRINTS'
 
